5d29887c541e80b8c3680542f7f6c174f8a7bf3e
Mihail Mikov <m.mikoff@gmail.com>
2016-07-20 01:26:52 +0300

    started schedule

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,37 @@
 const fs = require('fs'),
-      gulp = require('gulp'),
-      tap = require('gulp-tap'),
-      headerTemplate = fs.readFileSync('./templates/header-template.html'),
-      footerTemplate = fs.readFileSync('./templates/footer-template.html'),
-      deployPath = '/usr/share/nginx/html/';
-      //deployPath = '../Web/Fullstack/';
+  gulp = require('gulp'),
+  tap = require('gulp-tap'),
+  headerTemplate = fs.readFileSync('./templates/header-template.html'),
+  footerTemplate = fs.readFileSync('./templates/footer-template.html'),
+  deploymentMap = { local: '../Web/Fullstack/',
+                    aws: '/usr/share/nginx/html/'},
+  deployPath = deploymentMap[process.env.FULL_STACK_PATH || 'aws'];
 
 gulp.task('build-reveal', () => {
   const css = [
-    "./bower_components/reveal-js/css/reveal.min.css",
-    "./bower_components/reveal-js/css/theme/night.css",
-    "./bower_components/reveal-js/lib/css/zenburn.css"
-  ],
-      js = [
-          "./bower_components/reveal-js/lib/js/head.min.js",
-          "./bower_components/reveal-js/js/reveal.min.js",
-          "./bower_components/reveal-js/plugin/highlight/highlight.js"
-        ];
+      "./bower_components/reveal-js/css/reveal.min.css",
+      "./bower_components/reveal-js/css/theme/night.css",
+      "./bower_components/reveal-js/lib/css/zenburn.css"
+    ],
+    js = [
+      "./bower_components/reveal-js/lib/js/head.min.js",
+      "./bower_components/reveal-js/js/reveal.min.js",
+      "./bower_components/reveal-js/plugin/highlight/highlight.js"
+    ];
 
   gulp.src('./src/**/index.html')
-              .pipe(tap(file => {
-                const path = file.path.substr(file.path.indexOf('src') + 4),
-                      title = path.substr(0, path.indexOf('\\'));
+    .pipe(tap(file => {
+      const path = file.path.substr(file.path.indexOf('src') + 4),
+        title = path.substr(0, path.indexOf('\\'));
 
-                file.contents = Buffer.concat([
-                  new Buffer(headerTemplate.toString().replace('{{title}}', 'Full stack technologies: ' + title)),
-                  file.contents,
-                  footerTemplate
-                ]);
-              }))
-              .pipe(gulp.dest(deployPath));
+      file.contents = Buffer.concat([
+        new Buffer(headerTemplate.toString().replace('{{title}}', 'Full stack technologies: ' + title)),
+        file.contents,
+        footerTemplate
+      ]);
+    }))
+    .pipe(gulp.dest(deployPath));
 
-  gulp.src(js).pipe(gulp.dest(deployPath +'js/'));
+  gulp.src(js).pipe(gulp.dest(deployPath + 'js/'));
   gulp.src(css).pipe(gulp.dest(deployPath + 'css/'));
 });
