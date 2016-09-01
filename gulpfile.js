6aa631d3f3b5ed169b7f65f9fddc24a1766dc663
Mihail Mikov <mihail.mikov@skyscanner.net>
2016-09-01 18:43:03 +0300

    gulp build extended

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,35 @@
-const fs = require('fs'),
-  gulp = require('gulp'),
-  tap = require('gulp-tap'),
-  headerTemplate = fs.readFileSync('./templates/header-template.html'),
-  footerTemplate = fs.readFileSync('./templates/footer-template.html'),
-  deploymentMap = { local: '../Web/Fullstack/',
-                    aws: '/usr/share/nginx/html/'
-                  },
-  deployPath = deploymentMap[process.env.FULL_STACK_PATH || 'aws'];
+'use strict';
 
-gulp.task('build-reveal', () => {
-  const css_libs = [
-      "./bower_components/reveal-js/css/reveal.min.css",
-      "./bower_components/reveal-js/css/theme/night.css",
-      "./bower_components/reveal-js/lib/css/zenburn.css"
-    ],
-    js_libs = [
-      "./bower_components/reveal-js/lib/js/head.min.js",
-      "./bower_components/reveal-js/js/reveal.min.js",
-      "./bower_components/reveal-js/plugin/highlight/highlight.js"
-    ];
+const gulp = require('gulp'),
+  clean = require('gulp-clean'),
+  helpers = require('./gulpHelpers'),
+  path = require('path'),
+  data = helpers.commonData,
 
-  gulp.src('./src/**/index.html')
-    .pipe(tap(file => {
-      const path = file.path.substr(file.path.indexOf('src') + 4),
-        title = path.substr(0, path.lastIndexOf('/'));
-        console.log(path, title);
+  buildAndDeploy = deployPath => () => {
+    const modules = [];
 
-      file.contents = Buffer.concat([
-        new Buffer(headerTemplate.toString().replace('{{title}}', 'Full stack technologies: ' + title)),
-        file.contents,
-        footerTemplate
-      ]);
-    }))
-    .pipe(gulp.dest(deployPath));
+      gulp.src('./src/*/index.html')
+        .pipe(helpers.extractModuleNames(modules)).on('end', () => {
+          gulp.src('./src/index.html')
+            .pipe(helpers.wrapModule(modules))
+            .pipe(gulp.dest(deployPath));
 
-  gulp.src(js_libs).pipe(gulp.dest(deployPath + 'js/'));
-  gulp.src(css_libs).pipe(gulp.dest(deployPath + 'css/'));
-});
+          gulp.src('./src/*/index.html')
+            .pipe(helpers.wrapModule(modules, data.titleHeading, data.homeLink))
+            .pipe(gulp.dest(deployPath));
+
+        }).on('error', err => console.log(err));
+
+      gulp.src(data.js_libs).pipe(gulp.dest(deployPath + 'js' + path.sep)).on('error', err => console.log(err));
+      gulp.src(data.css_libs).pipe(gulp.dest(deployPath + 'css' + path.sep)).on('error', err => console.log(err));
+      gulp.src(data.images).pipe(gulp.dest(deployPath + 'images' + path.sep)).on('error', err => console.log(err));
+  };
+
+gulp.task('build-reveal', buildAndDeploy('/usr/share/nginx/html/'));
+
+gulp.task('build-home', buildAndDeploy('C:\\MixWay\\Web\\Fullstack\\'));
+
+gulp.task('build-work', buildAndDeploy('C:\\Users\\mihailmikov\\HobbyProjects\\fullstack_local\\'));
+
+gulp.task('default', ['build-reveal']);
